4d326d10817c0e008df0f1c6ee762116f5c87f84
Lukas Kern <lukask@ppedv.de>
2022-02-25 17:01:05 +0100

    Linq verbessert

--- M011_GenericsListen/Linq.cs
+++ M011_GenericsListen/Linq.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using static M011_GenericsListen.Linq.Fahrzeug;
 
 namespace M011_GenericsListen;
 
@@ -7,7 +6,7 @@ public class Linq
 {
 	public static void Main(string[] args)
 	{
-		#region Simple Linq
+		#region Einfaches Linq
 		//Erstellt eine Liste von Start mit einer bestimmten Anzahl Elemente
 		//(5, 20): gibt eine Liste mit 5-24 zurück (da Start 5 und insgesamt 20 Elemente)
 		List<int> linqTest = Enumerable.Range(1, 20).ToList();
@@ -21,95 +20,130 @@ public class Linq
 
 		Console.WriteLine(linqTest.Sum());
 
-		Console.WriteLine(linqTest.First()); //Erstes Element der Liste
-		Console.WriteLine(linqTest.Last()); //Exception wenn Liste leer
+		Console.WriteLine(linqTest.First()); //Erstes Element der Liste, Exception wenn Liste leer
+		Console.WriteLine(linqTest.FirstOrDefault()); //null zurück wenn Liste leer
 
-		Console.WriteLine(linqTest.FirstOrDefault()); //Letztes Element der Liste
+		Console.WriteLine(linqTest.Last()); //Letztes Element der Liste, Exception wenn Liste leer
 		Console.WriteLine(linqTest.LastOrDefault()); //null zurück wenn Liste leer
 
-		//List<double> doubleList = linqTest.Cast<double>().ToList(); //Cast -> wandelt alle Elemente in einer Liste zu einem anderen Typen um und macht eine neue Liste
+		//Cast -> wandelt alle Elemente in einer Liste zu einem anderen Typen um und macht eine neue Liste
+		//List<long> doubleList = linqTest.Cast<long>().ToList();
 
 		IEnumerable<int[]> chunk = linqTest.Chunk(5); //Teilt Liste in 5er Teile auf
 		#endregion
 
-		List<Fahrzeug> fahrzeuge = new List<Fahrzeug>();
-		fahrzeuge.Add(new Fahrzeug(250, "BMW"));
-		fahrzeuge.Add(new Fahrzeug(270, "BMW"));
-		fahrzeuge.Add(new Fahrzeug(200, "VW"));
-		fahrzeuge.Add(new Fahrzeug(180, "Audi"));
-		fahrzeuge.Add(new Fahrzeug(250, "VW"));
-		fahrzeuge.Add(new Fahrzeug(300, "BMW"));
-		fahrzeuge.Add(new Fahrzeug(150, "VW"));
-		fahrzeuge.Add(new Fahrzeug(250, "VW"));
-		fahrzeuge.Add(new Fahrzeug(210, "Audi"));
+		List<Fahrzeug> fahrzeuge = new List<Fahrzeug>
+		{
+			new Fahrzeug(250, FahrzeugMarke.BMW),
+			new Fahrzeug(270, FahrzeugMarke.BMW),
+			new Fahrzeug(200, FahrzeugMarke.Audi),
+			new Fahrzeug(180, FahrzeugMarke.Audi),
+			new Fahrzeug(250, FahrzeugMarke.VW),
+			new Fahrzeug(300, FahrzeugMarke.BMW),
+			new Fahrzeug(150, FahrzeugMarke.VW),
+			new Fahrzeug(250, FahrzeugMarke.VW),
+			new Fahrzeug(210, FahrzeugMarke.Audi)
+		};
 
 		#region BMWs filtern mit Schleife
 		List<Fahrzeug> bmwsForEach = new List<Fahrzeug>();
 		foreach (Fahrzeug auto in fahrzeuge)
-			if (auto.Bezeichnung == "BMW")
+			if (auto.Marke == FahrzeugMarke.BMW)
 				bmwsForEach.Add(auto);
 		#endregion
 
 		#region BMWs filtern mit SQL-Schreibweise
 		List<Fahrzeug> bmws =  (from auto in fahrzeuge 
-								where auto.Bezeichnung == "BMW"
+								where auto.Marke == FahrzeugMarke.BMW
 								select auto).ToList(); //Für ToList() keine eigene Funktionalität
 		#endregion
 
 		#region BMWs filtern mit Methodenketten
-		List<Fahrzeug> bmwsNeu = fahrzeuge.Where(auto => auto.Bezeichnung == "BMW").ToList();
+		List<Fahrzeug> bmwsNeu = fahrzeuge.Where(auto => auto.Marke == FahrzeugMarke.BMW).ToList();
 		#endregion
 
-		Console.WriteLine(fahrzeuge.Max(auto => auto.MaxGeschwindigkeit)); //Holt die höchste Geschwindigkeit
+		#region Einfache Linq-Statements mit einer Objektliste
+		//Holt die höchste Geschwindigkeit (selbiges w.o. mit Min und Average)
+		Console.WriteLine(fahrzeuge.Max(auto => auto.MaxGeschwindigkeit));
 
 		//OrderBy sortiert aufsteigend
-		Fahrzeug fzg = fahrzeuge.OrderByDescending(auto => auto.MaxGeschwindigkeit).First(); //Sortiere nach Geschwindigkeit und nehme das erste Element
-		Console.WriteLine($"Das schnellste Fahrzeug ist ein {fzg.Bezeichnung} und die Höchstgeschwindigkeit ist {fzg.MaxGeschwindigkeit}");
-	
-		List<string> alleBez = fahrzeuge.Select(auto => auto.Bezeichnung).ToList(); //Holt alle Autobezeichnungen
+		//Sortiere nach Geschwindigkeit und nehme das erste Element
+		Fahrzeug fzg = fahrzeuge.OrderByDescending(auto => auto.MaxGeschwindigkeit).First();
+		Console.WriteLine($"Das schnellste Fahrzeug ist ein {fzg.Marke} und die Höchstgeschwindigkeit ist {fzg.MaxGeschwindigkeit}");
 
-		List<string> distinct = alleBez.Distinct().ToList(); //Filtert Elemente sodass jedes Element einzigartig ist
+		//Speichert alle vorhandenen Automarken
+		List<FahrzeugMarke> alleBez = fahrzeuge.Select(auto => auto.Marke).ToList();
 
-		int summeGeschwindigkeit = fahrzeuge.Sum(auto => auto.MaxGeschwindigkeit); //Summiert alle Geschwindigkeit
+		//Filtert Elemente sodass jedes Element einzigartig ist
+		List<FahrzeugMarke> distinct = alleBez.Distinct().ToList();
 
-		int summeVWs = fahrzeuge.Where(auto => auto.Bezeichnung == "VW" && auto.MaxGeschwindigkeit >= 200)
+		//Summiert alle Geschwindigkeiten
+		int summeGeschwindigkeiten = fahrzeuge.Sum(auto => auto.MaxGeschwindigkeit);
+
+		int summeVWs = fahrzeuge.Where(auto => auto.Marke == FahrzeugMarke.VW && auto.MaxGeschwindigkeit >= 200)
 								.Sum(e => e.MaxGeschwindigkeit); //Mehrere Bedingungen mit &&
 
-		bool alleAutosSchnell = fahrzeuge.All(auto => auto.MaxGeschwindigkeit >= 150); //Checkt ob alle Autos mindestens 150 fahren können
+		//Checkt ob alle Autos mindestens 150 fahren können
+		bool alleAutosSchnell = fahrzeuge.All(auto => auto.MaxGeschwindigkeit >= 150);
 
-		bool einAutoSchnell = fahrzeuge.Any(auto => auto.MaxGeschwindigkeit >= 250); //Checkt ob ein Auto die Bedingung erfüllt
+		//Checkt ob mindestens ein Fahrzeug in der Liste ist
+		bool mindestensEinFahrzeug = fahrzeuge.Any();
 
+		//Checkt ob ein Auto die Bedingung erfüllt
+		bool einAutoSchnell = fahrzeuge.Any(auto => auto.MaxGeschwindigkeit >= 250);
+
+		//Voller Typ: IEnumerable<IGrouping<FahrzeugMarke, Fahrzeug>>
+		//Gruppiert alle Fahrzeuge nach der Bezeichnung
+		var groupedFahrzeuge = fahrzeuge.GroupBy(e => e.Marke);
+
+		//fahrzeuge.Concat() -> Verbindet zwei Listen
+		//fahrzeuge.Append() und fahrzeuge.Prepend() fügen Elemente am Anfang/Ende hinzu
+		#endregion
+
+		#region Erweiterte Linq-Statements mit einer Objektliste
+		//Paging: Zeige nur eine fixe Anzahl an Elementen
 		List<Fahrzeug> pagedFahrzeuge;
 		int maxPages = 3;
-		for (int i = 0; i < 3; i++) //Paging: Zeige nur eine fixe Anzahl an Elementen
-			pagedFahrzeuge = fahrzeuge.Skip(i * maxPages).Take(3).ToList();
+
 		//Skip: überspringt Elemente vom Anfang der Liste
 		//Take: Nimmt entsprechend viele Elemente vom Anfang der Liste
+		for (int i = 0; i < maxPages; i++)
+			pagedFahrzeuge = fahrzeuge.Skip(i * maxPages).Take(3).ToList();
 
-		//Dictionary<string, int> maxGeschwindigkeit = fahrzeuge //ToDictionary: Zwei Parameter mit zweimal => und Beistrich getrennt
-		//	.ToDictionary(auto => auto.Bezeichnung, auto => auto.MaxGeschwindigkeit);
+		//ToDictionary: Zwei Parameter mit zweimal => und Beistrich getrennt
+		//Gibt die maximale Geschwindigkeit pro Automarke zurück
+		Dictionary<FahrzeugMarke, int> maxGeschwindigkeit = fahrzeuge
+			.GroupBy(auto => auto.Marke)
+			.ToDictionary(auto => auto.Key, auto => auto.Select(e => e.MaxGeschwindigkeit).Max());
 
-		string agg = fahrzeuge.Aggregate("", (str, fzg) => //Aggregiert Code auf einen Aggregator (string) (Aggregator = str)
-			str + $"Bezeichnung: {fzg.Bezeichnung}, Maximalgeschwindigkeit: {fzg.MaxGeschwindigkeit}\n");
+		//Aggregiert Code auf einen Aggregator (string) (Aggregator = str)
+		//Nützlich um eine Liste schön formatiert auszugeben
+		string agg = fahrzeuge.Aggregate("", (str, fzg) =>
+			str + $"Bezeichnung: {fzg.Marke}, Maximalgeschwindigkeit: {fzg.MaxGeschwindigkeit}\n");
 		Console.WriteLine(agg);
 
-		fahrzeuge.Aggregate(0, (sum, fzg) => sum += fzg.MaxGeschwindigkeit); //Maximalgeschwindigkeit addieren (Aggregator = sum)
-
-		//fahrzeuge.Concat -> Verbindet zwei Listen
-		var x = fahrzeuge.GroupBy(e => e.Bezeichnung); //Gruppiert alle Fahrzeuge nach der Bezeichnung
-		//fahrzeuge.Append() und fahrzeuge.Prepend() fügen Elemente am Anfang/Ende hinzu
+		//Maximalgeschwindigkeit addieren (Aggregator = sum)
+		fahrzeuge.Aggregate(0, (sum, fzg) => sum += fzg.MaxGeschwindigkeit);
+		#endregion
 	}
 
 	public class Fahrzeug
 	{
 		public int MaxGeschwindigkeit;
 
-		public string Bezeichnung;
+		public FahrzeugMarke Marke;
 
-		public Fahrzeug(int v, string b)
+		public Fahrzeug(int v, FahrzeugMarke fm)
 		{
 			MaxGeschwindigkeit = v;
-			Bezeichnung = b;
+			Marke = fm;
+		}
+
+		public enum FahrzeugMarke
+		{
+			BMW,
+			Audi,
+			VW
 		}
 	}
 }
